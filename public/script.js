8198d09c620c960f4ec5b4f11ecd25d49b5195a3
DeekshitaV <deekshitaverma@gmail.com>
2021-07-07 11:37:35 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -111,7 +111,7 @@ var peer = new Peer( undefined , {
               }</span> </b>
               <span>${message}</span>
           </div>`;
-        scrollToBottom;  
+        scrollToBottom();  
     });
 
     const scrollToBottom = () => {
